58862b54342bd071e77e1e79ff1d288d9b1d9c9c
550975101 <550975101@qq.com>
2020-11-30 19:21:31 +0800

    splitter练习

--- src/main/java/com/xxx/guava/test/splitter/SplitterTest.java
+++ src/main/java/com/xxx/guava/test/splitter/SplitterTest.java
@@ -7,8 +7,13 @@ import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.regex.Pattern;
 
 public class SplitterTest {
+
+  Pattern pattern = Pattern.compile("[.|,]");
+
   @Test
   public void splitStringToIterableWithDelimiter() {
     /*通过Char初始化拆分器，将String分隔为Iterable*/
@@ -22,4 +27,51 @@ public class SplitterTest {
     list1.forEach(System.out::println);
   }
 
+  @Test
+  public void splitStringToListWithDelimiter() {
+    /*通过Char初始化拆分器，将String直接分隔为List*/
+    String str = "this, is  , , random , text,";
+    List<String> list = Splitter.on(",").omitEmptyStrings().trimResults().splitToList(str);
+    list.forEach(System.out::println);
+    //测试添加、移除  抛出 UnsupportedOperationException 看来分割后不支持 添加移除
+    list.add("aaa");
+    list.remove("aaa");
+  }
+
+  @Test
+  public void splitStringToListWithCharMatcher() {
+    /*通过CharMatcher初始化拆分器*/
+    String str = "a,b;c.d,e.f)))),g,h.i;j.1,2.3;";
+    List<String> list = Splitter.on(CharMatcher.anyOf(";.,)")).omitEmptyStrings().trimResults().splitToList(str);
+    list.forEach(System.out::println);
+    System.out.println(list.size());
+  }
+
+  @Test
+  public void splitStringToListWithRegularExpression() {
+    /*通过正则表达式初始化拆分器*/
+    String str = "apple.banana,,orange,,.";
+//    List<String> list = Splitter.on(Pattern.compile("[.|,]")).omitEmptyStrings().trimResults().splitToList(str);
+    List<String> list = Splitter.on(pattern).omitEmptyStrings().trimResults().splitToList(str);
+//    List<String> list = Splitter.onPattern("[.|,]").omitEmptyStrings().trimResults().splitToList(str);
+    list.forEach(System.out::println);
+    System.out.println(list.size());
+  }
+
+  @Test
+  public void splitStringToListWithFixedLength() {
+    /*将字符串分割为元素长度固定的List，最后一个元素长度不足可以直接返回*/
+    String str = "Hello world";
+    List<String> list = Splitter.fixedLength(3).splitToList(str);
+    list.forEach(System.out::println);
+    System.out.println(list.size());
+  }
+
+  @Test
+  public void splitStringToMap() {
+    /*String转Map*/
+    String str = "John=first,Adam=second";
+    Map<String, String> split = Splitter.on(",").withKeyValueSeparator("=").split(str);
+    split.forEach((s, s2) -> System.out.println("key: "+s+" value: "+s));
+  }
 }
