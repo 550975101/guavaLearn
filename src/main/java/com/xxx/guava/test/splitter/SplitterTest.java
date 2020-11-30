21dff33066fafad345bec1a795ea53b417e91cf1
550975101 <550975101@qq.com>
2020-11-30 18:33:04 +0800

    splitter练习

--- src/main/java/com/xxx/guava/test/splitter/SplitterTest.java
+++ src/main/java/com/xxx/guava/test/splitter/SplitterTest.java
@@ -1,4 +1,18 @@
 package com.xxx.guava.test.splitter;
 
+import com.google.common.base.Splitter;
+import com.google.common.collect.Lists;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
 public class SplitterTest {
+  @Test
+  public void splitStringToIterableWithDelimiter() {
+    /*通过Char初始化拆分器，将String分隔为Iterable*/
+    String str = "this, is  , , random , text,";
+    ArrayList<String> list = Lists.newArrayList(Splitter.on(",").omitEmptyStrings().trimResults().split(str));
+    list.forEach(System.out::println);
+  }
+
 }
